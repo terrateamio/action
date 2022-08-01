195cbc21491f2938d17b3c2962c2b262cbd8e765
Malcolm <malcolm@terrateam.io>
2022-08-01 17:06:17 +0200

    FIX Add more error logging and fix when infracost results are None

--- terrat_runner/infracost.py
+++ terrat_runner/infracost.py
@@ -4,3 +4,13 @@ import hashlib
 def json_filename_of_dirspace(dirspace):
     filename_str = ','.join([dirspace['path'], dirspace['workspace']]).encode('utf-8')
     return hashlib.sha256(filename_str).hexdigest() + '.json'
+
+
+def convert_cost(cost):
+    if cost is not None:
+        try:
+            return float(cost)
+        except ValueError:
+            return 0.0
+    else:
+        return 0.0
