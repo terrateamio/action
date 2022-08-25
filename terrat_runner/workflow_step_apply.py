5e5dc1236df4c6957de51201d2a6d50948dbfeb3
Malcolm <malcolm@terrateam.io>
2022-08-25 17:31:35 +0200

    FIX Typo in name

--- terrat_runner/workflow_step_apply.py
+++ terrat_runner/workflow_step_apply.py
@@ -7,7 +7,7 @@ def run(state, config):
     config['args'] = ['apply', '$TERRATEAM_PLAN_FILE']
     config['output_key'] = 'apply'
     result = workflow_step_terraform.run(state, config)
-    return workflow.Return(failed=result.failed,
+    return workflow.Result(failed=result.failed,
                            state=result.state,
                            workflow_step={'type': 'apply'},
                            outputs=result.outputs)
