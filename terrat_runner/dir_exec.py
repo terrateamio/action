c13fd3795de6976ee6120a58d76e8ac30af62b5b
Malcolm <malcolm@terrateam.io>
2022-12-01 11:21:21 +0100

    PRO-227 FIX Do not run multiple workspaces for the same dir in parallel

--- terrat_runner/dir_exec.py
+++ terrat_runner/dir_exec.py
@@ -1,14 +1,46 @@
 import multiprocessing
 
 
+# Need to order dirs by rank, but also, want to run one workspace at a time for
+# a dir.  So we fake it by increasing the rank on dirs that have multiple
+# workspaces.
 def _order_dirs_by_rank(dirs):
-    ret = {}
+    ranking = {}
 
     for d in dirs:
-        ret.setdefault(d['rank'], []).append(d)
+        ranking.setdefault(d['rank'], {}).setdefault(d['path'], []).append(d)
 
-    return [sorted(ret[k], key=lambda d: (d['path'], d['workspace']))
-            for k in sorted(ret.keys())]
+    # ranking is a dict where the key is the numerical rank and the value is a
+    # dictionary of the dir path to all entries.  There will only be multiple
+    # entries for the path if there are multiple workspaces.
+    #
+    # We iterate the ranking, in order, and then for each list of values, we
+    # consume any dirspaces that we can and at the end of each iteration add the
+    # row to the final result.  Once there are no more values to add, then
+    # [added] will be false, and we exit.
+    #
+    # For example, if we have dirs [(1, d1, w1), (1, d1, w2), (2, d2,w1)]
+    #
+    # ranking would be {1: {d1: [(1, d1, w1), (1, d1, w2)]}, 2: [(2, d2, w1)]}
+    #
+    # Then the result would be [[(1, d1, w1)], [(1, d1, w2)], [(2, d2, w1)]]
+    ret = []
+    for k in sorted(ranking.keys()):
+        row = []
+        vs = ranking[k].values()
+
+        added = True
+        while added:
+            added = False
+            for v in vs:
+                if v:
+                    row.append(v.pop())
+                    added = True
+
+            ret.append(row)
+            row = []
+
+    return ret
 
 
 def _run(args):
