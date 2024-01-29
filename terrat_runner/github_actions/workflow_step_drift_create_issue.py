bfe866c24ab8deedb58a5bece2c2c6d49e1c8c72
Malcolm <malcolm@terrateam.io>
2024-01-29 14:46:12 +0100

    PRO-456 FIX Large drift output

--- terrat_runner/github_actions/workflow_step_drift_create_issue.py
+++ terrat_runner/github_actions/workflow_step_drift_create_issue.py
@@ -82,6 +82,18 @@ Report ID: {report_id}
 '''.format(header=ISSUE_HEADER, output=all_dirspace_plan_output, report_id=report_id))
 
 
+def drift_output_too_long(env, report_id):
+    return ('''
+{header}
+Drift output too large to display.  See action logs [here](https://github.com/{repo}/actions/runs/{run_id}).
+---
+Report ID: {report_id}
+''').format(header=ISSUE_HEADER,
+            repo=env['GITHUB_REPOSITORY'],
+            run_id=env['GITHUB_RUN_ID'],
+            report_id=report_id)
+
+
 def find_matching_issue(env, report_id):
     report_id_line = 'Report ID: ' + report_id
     url = 'https://api.github.com/repos/{repo}/issues'.format(
@@ -98,17 +110,57 @@ def find_matching_issue(env, report_id):
                 return issue
 
 
+def compact_issue_body(issue_body):
+    return '\n'.join([
+        line
+        for line in issue_body.splitlines()
+        if ' Refreshing state...' not in line and '= (known after apply)' not in line
+    ])
+
+
+def body_is_too_long(body):
+    errors = body.get('errors', [])
+    for err in errors:
+        if 'message' in err and err['message'].startswith('body is too long'):
+            return True
+
+    return False
+
+
+def create_issue(state, report_id, issue_body, compact_view=False):
+    if compact_view:
+        issue_body = compact_issue_body(issue_body)
+
+    url = 'https://api.github.com/repos/{repo}/issues'.format(repo=state.env['GITHUB_REPOSITORY'])
+    headers = {
+        'User-Agent': 'Terrateam Action',
+        'Authorization': 'token ' + state.env['TERRATEAM_GITHUB_TOKEN']}
+    issue = {
+        'title': TITLE,
+        'body': issue_body
+    }
+    ret = requests_retry.post(url, headers=headers, json=issue)
+    if ret.status_code == 201:
+        logging.info('DRIFT_CREATE_ISSUE : SUCCESS')
+    elif ret.status_code == 422 and body_is_too_long(ret.json()) and not compact_view:
+        return create_issue(state, report_id, issue_body, compact_view=True)
+    elif ret.status_code == 422 and body_is_too_long(ret.json()):
+        return create_issue(state, report_id, drift_output_too_long(state.env, report_id))
+    else:
+        logging.error('Failed to make issue: %s', ret.text)
+        raise Exception('Failed to make issue')
+
+
 def maybe_create_issue(state):
     run_kind = state.env['TERRATEAM_RUN_KIND']
     results_file = state.env['TERRATEAM_RESULTS_FILE']
     all_dirspace_plan_output = ''
     if run_kind == 'drift' and os.path.isfile(results_file):
         dirspace_plans = extract_dirspace_plans(state.env['TERRATEAM_RESULTS_FILE'])
-        dirspaces_with_changes = [d for d in dirspace_plans if d['has_changes']]
+        dirspaces_with_changes = [d for d in dirspace_plans if d['has_changes'] in [True, None]]
         if dirspaces_with_changes:
             all_dirspace_plan_output = format_dirspaces(dirspaces_with_changes)
             report_id = hashlib.md5(all_dirspace_plan_output.encode('utf-8')).hexdigest()
-
             state = state.run_time.update_authentication(state)
 
             existing_issue = find_matching_issue(state.env, report_id)
@@ -116,18 +168,7 @@ def maybe_create_issue(state):
                 logging.info('DRIFT_CREATE_ISSUE : ISSUE_EXISTS : %s', existing_issue['id'])
             else:
                 issue_body = format_issue_body(all_dirspace_plan_output, report_id)
-                url = 'https://api.github.com/repos/{repo}/issues'.format(repo=state.env['GITHUB_REPOSITORY'])
-                headers = {
-                    'User-Agent': 'Terrateam Action',
-                    'Authorization': 'token ' + state.env['TERRATEAM_GITHUB_TOKEN']}
-                issue = {
-                    'title': TITLE,
-                    'body': issue_body
-                }
-                ret = requests_retry.post(url, headers=headers, json=issue)
-                if ret.status_code != 201:
-                    logging.error('Failed to make issue: %s', ret.text)
-                    raise Exception('Failed to make issue')
+                create_issue(state, report_id, issue_body)
 
 
 def run(state, config):
