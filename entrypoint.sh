9eeee321f695a8c57ad201e45411f4423c9dc363
root <bender@terrateam.io>
2024-06-21 11:16:44 +0200

    Automatically run http-proxy-add-self-signed-certs based on env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 export PATH="$PATH":/usr/local/proxy/bin
+
+if [ -n "$HTTPS_PROXY" ] && [ -n "$HTTPS_PROXY_DOMAINS" ]; then
+  /usr/local/bin/http-proxy-add-self-signed-certs
+fi
 
 WORK_TOKEN="$1"
 API_BASE_URL="$2"
 
+# Ensure required environment variables are set
+if [ -z "$GITHUB_WORKSPACE" ] || [ -z "$GITHUB_RUN_ID" ] || [ -z "$GITHUB_SHA" ]; then
+  echo "Error: Missing required environment variables."
+  exit 1
+fi
+
 echo "Starting Terrat Runner"
-ssh-agent python3 /terrat_runner/main.py \
+
+eval "$(ssh-agent -s)"
+ssh-add
+
+python3 /terrat_runner/main.py \
         --work-token "$WORK_TOKEN" \
         --workspace "$GITHUB_WORKSPACE" \
         --api-base-url "$API_BASE_URL" \
