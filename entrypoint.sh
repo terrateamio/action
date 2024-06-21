f897537a3b2a39649dda8bee902f987e0f183170
root <bender@terrateam.io>
2024-06-21 11:36:15 +0200

    Revert ssh entrypoint modifications

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,16 @@
-#!/usr/bin/env bash
-set -x
+#! /bin/sh
 
 export PATH="$PATH":/usr/local/proxy/bin
-
-if [ -n "$HTTPS_PROXY" ] && [ -n "$HTTPS_PROXY_DOMAINS" ]; then
-  /usr/local/bin/http-proxy-add-self-signed-certs
-fi
 
 WORK_TOKEN="$1"
 API_BASE_URL="$2"
 
-# Ensure required environment variables are set
-if [ -z "$GITHUB_WORKSPACE" ] || [ -z "$GITHUB_RUN_ID" ] || [ -z "$GITHUB_SHA" ]; then
-  echo "Error: Missing required environment variables."
-  exit 1
+if [ -n "$HTTP_PROXY" ] && [ -n "$HTTP_PROXY_DOMAINS" ]; then
+  /usr/local/bin/http-proxy-add-self-signed-certs
 fi
 
 echo "Starting Terrat Runner"
-
-eval "$(ssh-agent -s)"
-ssh-add
-
-python3 /terrat_runner/main.py \
+ssh-agent python3 /terrat_runner/main.py \
         --work-token "$WORK_TOKEN" \
         --workspace "$GITHUB_WORKSPACE" \
         --api-base-url "$API_BASE_URL" \
