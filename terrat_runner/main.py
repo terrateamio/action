9627299569ec49f197e4a6cb3ab8b3b0a215b193
Malcolm <malcolm@terrateam.io>
2024-09-27 13:32:44 +0100

    PRO-562 ADD Repo config debug output

--- terrat_runner/main.py
+++ terrat_runner/main.py
@@ -176,6 +176,8 @@ def run(args):
 
     logging.debug('LOADING: REPO_CONFIG')
     rc = wm['config']
+    logging.debug('REPO_CONFIG')
+    logging.debug('%s', json.dumps(rc, indent=2))
 
     run_time = github_actions.run_time.Run_time()
 
