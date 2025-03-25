811786bbed05eb64797c6048f5a04da133bbcd3b
bender2352 <165169475+bender2352@users.noreply.github.com>
2025-03-25 10:31:48 +0100

    Revert "Release v1"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /bin/sh
 
 # Set PATH
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -9,25 +9,6 @@ export PATH="$PATH":/usr/local/proxy/bin
 # Append $TERRATEAM_APPEND_PATH to $PATH
 if [ "$TERRATEAM_APPEND_PATH" ]; then
   export PATH="$PATH:$TERRATEAM_APPEND_PATH"
-fi
-
-# Install custom CA certificates
-has_custom_certs=false
-
-# Loop through all CUSTOM_CA_BUNDLE_* variables
-# Each variable must contain a single PEM-encoded certificate
-for var in "${!CUSTOM_CA_BUNDLE_@}"; do
-  cert="${!var}"
-  if [ -n "$cert" ]; then
-    file="/usr/local/share/ca-certificates/${var}.crt"
-    echo "$cert" > "$file"
-    echo "Wrote custom CA cert to $file"
-    has_custom_certs=true
-  fi
-done
-
-if [ "$has_custom_certs" = true ]; then
-  update-ca-certificates
 fi
 
 # Arguments: WORK_TOKEN and API_BASE_URL, passed during the action setup
