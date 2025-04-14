6ae032e6f56c8d652a3a85af55f1e8cf9492f6bf
bender2352 <165169475+bender2352@users.noreply.github.com>
2025-04-14 11:08:52 +0200

    430 REFACTOR remove flock from kubectl installer

--- kubectl-cli-setup.sh
+++ kubectl-cli-setup.sh
@@ -5,5 +5,5 @@ set -u
 
 KUBECTL_VERSION="$1"
 
-flock /tmp/kubectl.install curl -sSL -o /tmp/kubectl "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
-flock /tmp/kubectl.install install -m 0755 /tmp/kubectl /usr/bin/kubectl
+curl -sSL -o /tmp/kubectl "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+install -m 0755 /tmp/kubectl /usr/bin/kubectl
