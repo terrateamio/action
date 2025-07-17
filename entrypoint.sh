8ee484b93e1d90fdf20cf3c86116380cb51f2461
Malcolm <malcolm@terrateam.io>
2025-07-17 14:59:20 +0100

    #620 FIX Split gitlab entrypoint out so image can be run directly

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,25 +16,26 @@ if [ "$TERRATEAM_APPEND_PATH" ]; then
   export PATH="$PATH:$TERRATEAM_APPEND_PATH"
 fi
 
-# Arguments: WORK_TOKEN and API_BASE_URL, passed during the action setup
-WORK_TOKEN="$1"
-API_BASE_URL="$2"
+# Set PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-# Deprecated
-# WARNING: The following script, `unsafe-add-certs`, is insecure and should not be relied upon in production environments.
-# It is slated for removal in the future to avoid any potential security vulnerabilities.
-# If HTTPS_PROXY and HTTPS_PROXY_DOMAINS are set, proceed with caution.
-if [ -n "$HTTPS_PROXY" ] && [ -n "$HTTPS_PROXY_DOMAINS" ]; then
-  /usr/local/bin/unsafe-add-certs # temporary script; not recommended for production use
+# Adding /usr/local/proxy/bin to the PATH for accessing additional tools, if needed
+export PATH="$PATH":/usr/local/proxy/bin
+
+# Prepend $TERRATEAM_PREPEND_PATH to $PATH
+if [ "$TERRATEAM_PREPEND_PATH" ]; then
+  export PATH="$TERRATEAM_PREPEND_PATH:$PATH"
+fi
+
+# Append $TERRATEAM_APPEND_PATH to $PATH
+if [ "$TERRATEAM_APPEND_PATH" ]; then
+  export PATH="$PATH:$TERRATEAM_APPEND_PATH"
 fi
 
 export TENV_AUTO_INSTALL=true
 
-# Start the Terrat Runner with SSH agent
-echo "Starting Terrat Runner"
-ssh-agent python3 /terrat_runner/main.py \
-        --work-token "$WORK_TOKEN" \
-        --workspace "$GITHUB_WORKSPACE" \
-        --api-base-url "$API_BASE_URL" \
-        --run-id "$GITHUB_RUN_ID" \
-        --sha "$GITHUB_SHA"
+if [ "$TERRAT_VCS_PROVIDER" = "gitlab" ]; then
+   /entrypoint_gitlab.sh
+else
+    /entrypoint_github.sh "$1" "$2"
+fi
