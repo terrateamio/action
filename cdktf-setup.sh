7fe6645af34442938817191922827022dd5b0612
Josh Pollara <josh@terrateam.io>
2025-07-28 23:33:06 +0200

    #474 REFACTOR pull node from official repo

--- cdktf-setup.sh
+++ cdktf-setup.sh
@@ -5,7 +5,7 @@ set -e
 # Detect architecture and map to Node.js naming convention
 ARCH=$(uname -m | sed 's/x86_64/x64/g' | sed 's/aarch64/arm64/g')
 
-curl -fsSL -o /tmp/node-v18.14.0.tar.gz "https://github.com/terrateamio/packages/raw/main/node/node-v18.14.0-linux-${ARCH}.tar.gz"
+curl -fsSL -o /tmp/node-v18.14.0.tar.gz "https://nodejs.org/dist/v18.14.0/node-v18.14.0-linux-${ARCH}.tar.gz"
 tar -C /usr/local/ -xzf /tmp/node-v18.14.0.tar.gz
 ln -s /usr/local/node-v18.14.0-linux-${ARCH} /usr/local/node
 ln -s /usr/local/node/bin/* /usr/local/bin/
